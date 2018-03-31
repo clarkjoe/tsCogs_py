186bdd671f66f0516bfa1e0e13979f1082e9f0a9
clarkjoe <joeyclarktkd@gmail.com>
2018-03-30 18:48:14 -0600

    adding ladder function

--- sequences.py
+++ sequences.py
@@ -1,11 +1,11 @@
 import numpy as npy
 
 # Find longest sequence of zeros
-def findSignedSequence(vector, sign):
+def findSignedSequence(array, sign):
     poten_max = real_max = 0
 
-    if sign in set(npy.sign(vector)):
-        for number in npy.sign(vector):
+    if sign in set(npy.sign(array)):
+        for number in npy.sign(array):
             if number == sign:
                 poten_max += 1
             else:
@@ -15,3 +15,8 @@ def findSignedSequence(vector, sign):
     else:
         return 0
     return real_max
+
+# Find ladder sequences
+def findLadderSequence(array, seq):
+    max_Inc_Seq = max_Dec_Seq= max_Inc_Pos_Seq = max_Dec_Pos_Seq = max_Inc_Neg_Seq = max_Dec_Neg_Seq = 0
+    poten_Max_Inc_Seq = poten_Max_Dec_Seq = poten_Max_Inc_Pos_Seq = poten_Max_Dec_Pos_Seq = poten_Max_Inc_Neg_Seq = poten_Max_Dec_Neg_Seq = 0
