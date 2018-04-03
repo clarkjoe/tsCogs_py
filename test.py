4c02b191edc65fce9d9f468f50d42de5376f4ae5
clarkjoe <cla12026@byui.edu>
2018-04-03 16:12:44 -0600

    more updates

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import sequences
 import nest_generic
+import rpy2.robjects as robjects
+from rpy2.robjects import pandas2ri
+pandas2ri.activate()
 
-vec = [-1,0,1]
+readRDS = robjects.r['readRDS']
+df = readRDS('rawData.rds')
+df = pandas2ri.ri2py(df)
+
+print("This is the end")
 
 # var = sequences.findLadderSequence(vec, "IP")
 
-nest_generic.nest_core(1, 'string')
+# nest_generic.nest_core(1, 'string')
 # print (var)
