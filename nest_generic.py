7c150f924ee756cb76e323c1c80241161759421e
clarkjoe <joeyclarktkd@gmail.com>
2018-05-16 16:56:17 -0600

    updating dplython

--- nest_generic.py
+++ nest_generic.py
@@ -1,15 +1,16 @@
 import pandas
-from dplython import (diamonds, X, select, head)
-import dplython
+from dplython import (DplyFrame, X, diamonds, select, sift, sample_n,
+    sample_frac, head, arrange, mutate, group_by, summarize, DelayFunction) 
 
-def nest_core(data, type):
+def nest_core(data, timeType):
 
     type.capitalize()
     letter = type[:1]
 
-    print (1 + \
-        1)
-
-    diamonds >> \
-     select(X.carat, X.cut, X.price) >>\
-     head(5)
+    return data >> \
+        select(X.carat, X.cut, X.price) >> \
+        head(20) >> \
+        group_by(X.carat) >> \
+        summarize(Count = X.price.sum()) >> \
+        group_by(X.carat) >> \
+        summarize(join(timeType + '_Count') = X.Count.mean())
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import sequences
 import nest_generic
+from dplython import diamonds
 
 vec = [-1,0,1]
 
 # var = sequences.findLadderSequence(vec, "IP")
 
-nest_generic.nest_core(1, 'string')
-# print (var)
+var = nest_generic.nest_core(diamonds, 'string')
+print (var)
