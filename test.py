a47926c66e1d1a1ea239ecfa2a1cba816ea9c229
clarkjoe <joeyclarktkd@gmail.com>
2018-05-16 17:07:57 -0600

    trying to do better

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 import sequences
 import nest_generic
-<<<<<<< HEAD
 from dplython import diamonds
-=======
+
 import rpy2.robjects as robjects
 from rpy2.robjects import pandas2ri
 pandas2ri.activate()
->>>>>>> 4c02b191edc65fce9d9f468f50d42de5376f4ae5
+
 
 readRDS = robjects.r['readRDS']
 df = readRDS('rawData.rds')
@@ -16,10 +15,10 @@ print("This is the end")
 
 # var = sequences.findLadderSequence(vec, "IP")
 
-<<<<<<< HEAD
+
 var = nest_generic.nest_core(diamonds, 'string')
 print (var)
-=======
+
 # nest_generic.nest_core(1, 'string')
 # print (var)
->>>>>>> 4c02b191edc65fce9d9f468f50d42de5376f4ae5
+
